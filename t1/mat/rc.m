06d09dcba4b2ed37c1e5fcf347118c73faaf894d
josemiguelbento <78874002+josemiguelbento@users.noreply.github.com>
2021-03-14 23:30:54 +0000

    Add files via upload

--- t1/mat/rc.m
+++ t1/mat/rc.m
@@ -1,75 +1,77 @@
 close all
 clear all
 
-%%EXAMPLE SYMBOLIC COMPUTATIONS
+pkg load symbolic;
 
-pkg load symbolic
+% 95815
 
-syms t
-syms R
-syms C
-syms vi(t)
-syms vo(t)
-syms i(t)
-
-i(t)=C*diff(vo,t)
-
-printf("\n\nKVL equation:\n");
-
-vi(t) = R*i(t)+vo(t)
-
-syms vo_n(t) %natural solution
-syms vo_f(t) %forced solution
-
-printf("\n\nSolution is of the form");
-
-v(t) = vo_n(t) + vo_f(t)
-
-printf("\n\nNatural solution:\n");
-syms A
-syms wn
-
-vi(t) = 0 %no excitation
-i_n(t) = C*diff(vo_n, t)
+R1 = sym ('1.04606282456');
+R2 = sym ('2.00732621328');
+R3 = sym ('3.06060705885');
+R4 = sym ('4.07055531265');
+R5 = sym ('3.1225213804');
+R6 = sym ('2.06927045958');
+R7 = sym ('1.01531018068');
+Va = sym ('5.24359648479');
+Id = sym ('1.01891541651');
+Kb = sym ('7.0473187437');
+Kc = sym ('8.3479788681');
 
 
-printf("\n\n Natural solution is of the form");
-vo_n(t) = A*exp(wn*t)
+%Mesh Method---------------------------------------------
 
-R*i_n(t)+vo_n(t) == 0
+syms I_SE I_SD I_IE I_ID;
 
-R*C*wn*vo_n(t)+vo_n(t) == 0
+Eq_A = R1*I_SE + R3*(I_SE+I_SD) + R4*(I_SE+I_IE) == Va;
+Eq_C = R6*I_IE + R7*I_IE + R4*(I_IE+I_SE)== Kc*I_IE;
+Eq_B = I_SD == Kb*R3*(I_SE+I_SD);
+Eq_D = I_ID == Id;
 
-R*C*wn+1==0
-
-solve(ans, wn)
+sm = solve(Eq_A,Eq_B,Eq_C,Eq_D);
 
 
-%%EXAMPLE NUMERIC COMPUTATIONS
+%Mesh Method COMPUTATION----------------------------------
 
-R=1e3 %Ohm
-C=100e-9 %F
+I_SE = double(sm.I_SE) %we have to put double otherwise sm.I_SE will come out symbolic
+I_SD = double(sm.I_SD)
+I_IE = double(sm.I_IE)
+I_ID = double(sm.I_ID)
 
-f = 1000 %Hz
-w = 2*pi*f; %rad/s
+V1m = double(Va)
+V2m = V1m - double(R1) * I_SE
+V3m = V2m + double(R2) * I_SD
+V4m = double(R4) * (I_SE + I_IE)
+V5m = V4m + double(R5) * (I_ID-I_SD)
+V6m = V4m - double(Kc) * I_IE
+V7m = - double(R6) * I_IE
+V8m = V7m
 
-%time axis: 0 to 10ms with 1us steps
-t=0:1e-6:10e-3; %s
 
-Zc = 1/(j*w*C)
-Cgain = Zc/(R+Zc)
-Gain = abs(Cgain)
-Phase = angle(Cgain)
+%Nodal method
 
-vi = 1*cos(w*t);
-vo = Gain*cos(w*t+Phase);
+syms V0n V1n V2n V3n V4n V5n V6n V7n V8n
 
-hf = figure ();
-plot (t*1000, vi, "g");
-hold on;
-plot (t*1000, vo, "b");
+Eq_a = V0n == 0;
+Eq_b = V8n == V7n;
+Eq_0 = (V1n-V2n)/R1 +(V0n - V8n)/R6 + (V0n - V4n)/R4 == 0; 
+Eq_2 = (V2n-V4n)/R3 + (V2n-V3n)/R2 + (V2n-V1n)/R1 == 0;
+Eq_3 = - Kb*(V2n-V4n) + (V3n-V2n)/R2 == 0;
+Eq_5 = (V5n-V4n)/R5 + Kb*(V2n-V4n) - Id == 0;
+Eq_7 = (V7n-V6n)/R7 + (V7n - V0n)/R6 == 0;
+Eq_Va = V1n - V0n == Va;
+Eq_Vc = V4n - V6n == Kc * (V0n - V7n)/R6;
 
-xlabel ("t[ms]");
-ylabel ("vi(t), vo(t) [V]");
-print (hf, "forced.eps", "-depsc");
-%print forced.eps
+sn = solve(Eq_a,Eq_b,Eq_0,Eq_2,Eq_3,Eq_5,Eq_7,Eq_Va,Eq_Vc);
+
+
+%Nodal method COMPUTATION
+
+V0n = double(sn.V0n)
+V1n = double(sn.V1n)
+V2n = double(sn.V2n)
+V3n = double(sn.V3n)
+V4n = double(sn.V4n)
+V5n = double(sn.V5n)
+V6n = double(sn.V6n)
+V7n = double(sn.V7n)
+V8n = V7n
