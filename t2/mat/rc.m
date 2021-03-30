ee78ea4529a47cba64ec03ad223bd119ee7929fc
josemiguelbento <jose.miguel.bento@gmail.com>
2021-03-30 17:24:58 +0100

    pus os dados a imprimir automaticamente para diferentes inputs

--- t2/mat/rc.m
+++ t2/mat/rc.m
@@ -1,6 +1,29 @@
 close all
 clear all
 
+%data_gen
+
+datafile=fopen('data.txt','r');
+data=fscanf(datafile, '%s = %f', [3 inf]);
+data = data';
+fclose(datafile);
+
+simf=fopen('data_sim.txt','w');
+fprintf(simf, 'R1 1 2 %fk\n', data(1,3));
+fprintf(simf, 'R2 2 3 %fk\n', data(2,3));
+fprintf(simf, 'R3 2 5 %fk\n', data(3,3));
+fprintf(simf, 'R4 4 5 %fk\n', data(4,3));
+fprintf(simf, 'R5 5 6 %fk\n', data(5,3));
+fprintf(simf, 'R6 4 7 %fk\n', data(6,3));
+fprintf(simf, 'R7 7 8 %fk\n', data(7,3));
+fprintf(simf, 'Vs 1 4 DC %f\n', data(8,3));
+fprintf(simf, 'Vf 9 7 DC 0\n');
+fprintf(simf, 'C 6 8 %fu\n', data(9,3));
+fprintf(simf, 'Gb 6 3 2 5 %fm\n', data(10,3));
+fprintf(simf, 'Hd 5 8 Vf %fk\n', data(11,3));
+
+fclose(simf);
+
 %%EXAMPLE SYMBOLIC COMPUTATIONS
 
 pkg load symbolic
