4668e31bd0346ba612bd9f43c4d7619af1939b24
josemiguelbento <jose.miguel.bento@gmail.com>
2021-03-31 00:50:20 +0100

    meti octave a funcionar e o ngspice para a analise op parecida a do t1 para validar as contas

--- t2/mat/rc.m
+++ t2/mat/rc.m
@@ -1,6 +1,10 @@
 close all
 clear all
 
+pkg load symbolic;
+
+format long;
+
 %data_gen
 
 datafile=fopen('data.txt','r');
@@ -12,86 +16,68 @@ simf=fopen('data_sim.txt','w');
 fprintf(simf, 'R1 1 2 %fk\n', data(1,3));
 fprintf(simf, 'R2 2 3 %fk\n', data(2,3));
 fprintf(simf, 'R3 2 5 %fk\n', data(3,3));
-fprintf(simf, 'R4 4 5 %fk\n', data(4,3));
+fprintf(simf, 'R4 0 5 %fk\n', data(4,3));
 fprintf(simf, 'R5 5 6 %fk\n', data(5,3));
-fprintf(simf, 'R6 4 7 %fk\n', data(6,3));
+fprintf(simf, 'R6 0 4 %fk\n', data(6,3));
 fprintf(simf, 'R7 7 8 %fk\n', data(7,3));
-fprintf(simf, 'Vs 1 4 DC %f\n', data(8,3));
-fprintf(simf, 'Vf 9 7 DC 0\n');
+fprintf(simf, 'Vs 1 0 DC %f\n', data(8,3));
+fprintf(simf, 'Vf 4 7 DC 0\n');
 fprintf(simf, 'C 6 8 %fu\n', data(9,3));
 fprintf(simf, 'Gb 6 3 2 5 %fm\n', data(10,3));
 fprintf(simf, 'Hd 5 8 Vf %fk\n', data(11,3));
 
 fclose(simf);
 
-%%EXAMPLE SYMBOLIC COMPUTATIONS
 
-pkg load symbolic
+R1 = sym(sprintf('%.11f', data(1,3)));
+R2 = sym(sprintf('%.11f', data(2,3)));
+R3 = sym(sprintf('%.11f', data(3,3)));
+R4 = sym(sprintf('%.11f', data(4,3)));
+R5 = sym(sprintf('%.11f', data(5,3)));
+R6 = sym(sprintf('%.11f', data(6,3)));
+R7 = sym(sprintf('%.11f', data(7,3)));
+Vs = sym(sprintf('%.11f', data(8,3)));
+Vf = sym('0');
+C = sym(sprintf('%.11f', data(9,3)));
+Kb = sym(sprintf('%.11f', data(10,3)));
+Kd = sym(sprintf('%.11f', data(11,3)));
 
-syms t
-syms R
-syms C
-syms vi(t)
-syms vo(t)
-syms i(t)
+%%NODAL METHOD
+syms V0 V1 V2 V3 V4 V5 V6 V7 V8
 
-i(t)=C*diff(vo,t)
+Eq_0 = V0 == 0;
+Eq_f = V4 == V7;
+Eq_d = V5-V8 == Kd*(V0-V4)/R6;
+Eq_s = V1-V0 == Vs;
+Eq_2 = (V2-V1)/R1 + (V2-V5)/R3 + (V2-V3)/R2 == 0;
+Eq_3 = (V3-V2)/R2 - Kb*(V2-V5) == 0;
+Eq_5 = (V5-V2)/R3 + (V5-V0)/R4 + (V5-V6)/R5 + (V8-V7)/R7 == 0;
+Eq_6 = Kb*(V2-V5) + (V6-V5)/R5 == 0;
+Eq_7 = (V4-V0)/R6 + (V7-V8)/R7 == 0;
 
-printf("\n\nKVL equation:\n");
+sn = solve(Eq_0,Eq_f,Eq_d,Eq_s,Eq_2,Eq_3,Eq_5,Eq_6,Eq_7);
 
-vi(t) = R*i(t)+vo(t)
+diary "nodal_tab.tex"
+diary on
 
-syms vo_n(t) %natural solution
-syms vo_f(t) %forced solution
-
-printf("\n\nSolution is of the form");
-
-v(t) = vo_n(t) + vo_f(t)
-
-printf("\n\nNatural solution:\n");
-syms A
-syms wn
-
-vi(t) = 0 %no excitation
-i_n(t) = C*diff(vo_n, t)
-
-
-printf("\n\n Natural solution is of the form");
-vo_n(t) = A*exp(wn*t)
-
-R*i_n(t)+vo_n(t) == 0
-
-R*C*wn*vo_n(t)+vo_n(t) == 0
-
-R*C*wn+1==0
-
-solve(ans, wn)
+V0 = double(sn.V0)
+V1 = double(sn.V1)
+V2 = double(sn.V2)
+V3 = double(sn.V3)
+V4 = double(sn.V4)
+V5 = double(sn.V5)
+V6 = double(sn.V6)
+V7 = double(sn.V7)
+V8 = double(sn.V8)
 
 
-%%EXAMPLE NUMERIC COMPUTATIONS
+I_r1 = (V2 -V1)/double(R1)
+I_r2 = (V3 -V2)/double(R2)
+I_r3 = (V5 -V2)/double(R3)
+I_r4 = (V5 -V0)/double(R4)
+I_r5 = (V6 -V5)/double(R5)
+I_r6 = (V4 -V0)/double(R6)
+I_r7 = (V8 -V7)/double(R7)
+Gbn= I_r5
 
-R=1e3 %Ohm
-C=100e-9 %F
-
-f = 1000 %Hz
-w = 2*pi*f; %rad/s
-
-%time axis: 0 to 10ms with 1us steps
-t=0:1e-6:10e-3; %s
-
-Zc = 1/(j*w*C)
-Cgain = Zc/(R+Zc)
-Gain = abs(Cgain)
-Phase = angle(Cgain)
-
-vi = 1*cos(w*t);
-vo = Gain*cos(w*t+Phase);
-
-hf = figure ();
-plot (t*1000, vi, "g");
-hold on;
-plot (t*1000, vo, "b");
-
-xlabel ("t[ms]");
-ylabel ("vi(t), vo(t) [V]");
-print (hf, "forced.eps", "-depsc");
+diary off
