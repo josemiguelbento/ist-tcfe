4b459953320b73e58ed30e0cad64eb2f1802ed85
josemiguelbento <jose.miguel.bento@gmail.com>
2021-04-28 23:23:18 +0100

    meti o merit a imprimir para uma tabela

--- t3/eval_sim.m
+++ t3/eval_sim.m
@@ -15,17 +15,24 @@ dataf = fopen('./sim/result_sim.txt','r');
 DATA = fscanf(dataf,'%*s = %f');
 fclose(dataf);
 
-diary merit.txt
+diary merit.tex
 diary on
-Renv
-C
-Rreg
-n
-n_diodes
-cost = Renv/1000 + C*1e6 + Rreg/1000 + n_diodes*2*0.1+4*0.1
-average = DATA(1)
-ripple = DATA(4)
+printf('R_{env} & %dkohm\n',Renv/1000);
+printf('C_{env} & %duF\n', C*1e6);
+printf('R_{reg} & %dkohm\n', Rreg/1000);
+printf('n_{transformer} & %d\n', n);
+printf('n_{diodes} & %d\n', n_diodes);
 
-MERIT = 1/cost/(ripple+abs(average-12)+1e-6)
+cost = Renv/1000 + C*1e6 + Rreg/1000 + n_diodes*2*0.1+4*0.1;
+printf('Cost & %dMU\n', cost);
+
+average = DATA(1);
+ripple = DATA(4);
+
+MERIT = 1/cost/(ripple+abs(average-12)+1e-6);
+
+printf('Average & %dV\n', average);
+printf('Ripple & %dV\n', ripple);
+printf('MERIT & %d\n', MERIT);
 
 diary off
