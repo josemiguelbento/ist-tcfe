8510f4a54f7bf22f514f1555c89fa47b383f84f9
ThomChilds <thomasmchilds@gmail.com>
2021-04-30 15:17:11 -0400

    relatorio a 80 por cento. So faltam os graficos do octave mas o ze trata disso

--- t3/eval_sim.m
+++ t3/eval_sim.m
@@ -34,11 +34,11 @@ MERIT_oct = 1/cost/(ripple_oct+abs(average_oct-12)+1e-6);
 
 diary merit.tex
 diary on
-printf('R_{env} & %d & %d & kohm\n',Renv/1000,Renv/1000);
-printf('C_{env} & %d & %d & uF\n', C*1e6, C*1e6);
-printf('R_{reg} & %d & %d & kohm\n', Rreg/1000, Rreg/1000);
-printf('n_{transformer} & %.2f & %.2f & \n', n, n);
-printf('n_{diodes} & %d & %d & diodes\n', n_diodes, n_diodes);
+printf('$R_{env}$ & %d & %d & kohm\n',Renv/1000,Renv/1000);
+printf('$C_{env}$ & %d & %d & uF\n', C*1e6, C*1e6);
+printf('$R_{reg}$ & %d & %d & kohm\n', Rreg/1000, Rreg/1000);
+printf('$n_{transformer}$ & %.2f & %.2f & \n', n, n);
+printf('$n_{diodes}$ & %d & %d & diodes\n', n_diodes, n_diodes);
 printf('Cost & %d & %d & MU\n', cost, cost);
 printf('Average & %.3f & %.3f & V\n', average, average_oct);
 printf('Ripple & %.6f & %.6f & V\n', ripple, ripple_oct);
