ab51df945713c400101ead9b0e271b3b92a1700d
josemiguelbento <jose.miguel.bento@gmail.com>
2021-05-19 22:11:49 +0100

    teste a funcionar

--- t4/data_gen.m
+++ t4/data_gen.m
@@ -51,6 +51,35 @@ printf("RL out 0 %d\n", RL)
 
 diary off
 
+diary data_t4_inc.txt
+diary on
+
+printf("Vcc vcc 0 12.0\n")
+%printf("Vin in 0 0 ac 1.0 sin(0 %d %d)\n", Vinm, Vinf)
+%printf("Rin in in2 %d\n", Rin)
+printf("Vin in2 0 0\n")
+printf("* input coupling capacitor\n")
+printf("Ci in2 base %d\n", Ci)
+printf("* bias circuit\n")
+printf("R1 vcc base %d\n", R1)
+printf("R2 base 0 %d\n", R2)
+printf("* gain stage\n")
+printf("Q1 coll base emit BC547A\n")
+printf("Rc vcc coll %d\n", Rc)
+printf("Re emit 0 %d\n", Re)
+printf("* bypass capacitor\n")
+printf("Cb emit 0 %d\n", Cb)
+printf("* output stage\n")
+printf("Q2 0 coll emit2 BC557A\n")
+printf("Rout emit2 vcc %d\n", Rout)
+printf("* output coupling capacitor\n")
+printf("Co emit2 out %d\n", Co)
+%printf("* load\n")
+%printf("RL out 0 %d\n", RL)
+printf("Vo out 0 0 ac 1\n")
+
+diary off
+
 
 
 
